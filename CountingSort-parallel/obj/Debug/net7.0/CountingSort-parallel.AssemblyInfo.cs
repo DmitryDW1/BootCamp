f20347d1d7bd0e7a357a3bd89d1d88dd2a8e996a
DmitryDW <DmitryDW1@yandex.ru>
2023-03-07 22:17:28 +0600

    Add CountingSort-parallel

--- CountingSort-parallel/obj/Debug/net7.0/CountingSort-parallel.AssemblyInfo.cs
+++ CountingSort-parallel/obj/Debug/net7.0/CountingSort-parallel.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CountingSort")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("CountingSort-parallel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("CountingSort")]
-[assembly: System.Reflection.AssemblyTitleAttribute("CountingSort")]
+[assembly: System.Reflection.AssemblyProductAttribute("CountingSort-parallel")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CountingSort-parallel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
