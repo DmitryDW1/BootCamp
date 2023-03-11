dbd702f79ac5853dbc7bea88bd718aeb99c112ef
DmitryDW <DmitryDW1@yandex.ru>
2023-03-11 17:10:32 +0600

    Add file.txt

--- FLASK/html_css_jinja/main.py
+++ FLASK/html_css_jinja/main.py
@@ -1,21 +1,10 @@
-from flask import Flask, render_template
+file = open('file.txt', 'r' encoding='utf-8')
 
+print(file.read())
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def main():
-    user_name = ['Aleksey', 'Alla', 'Almir', 'Diana', 'Ilali']
-    return render_template('base.html', user_name=user_name,
-                             title='Главная страница')
-
-
-@app.route('/bootcamp')
-def boot():
-    return render_template('bootcamp.html', title='BootCamp-tasks')
-
-
-if __name__ == '__main__':
-    app.run()
-
+file.close()
+'''
+а - режим добавление
+w - режим на запись(очищает файл)
+r - режим считывания
+'''
